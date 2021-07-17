1a9241298fcd43e7e9c2858d7fce1959a58914a1
andt <andt1@runsystem.net>
2021-07-17 10:24:48 +0700

    first commit

--- App.js
+++ App.js
@@ -1,223 +1,197 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {Component} from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Dimensions,
+  Image,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
   TouchableOpacity,
-  Alert,
+  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import CodePush from 'react-native-code-push';
-import * as Progress from 'react-native-progress';
-import RNRestart from 'react-native-restart';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-let totalPercent = 0;
-class App extends Component {
+class App extends Component<{}> {
   constructor() {
     super();
     this.state = {
-      loading: false,
+      restartAllowed: true,
+      syncMessage: '',
     };
   }
 
-  onSyncStatusChange = SyncStatus => {
-    switch (SyncStatus) {
-      case SyncStatus.CHECKING_FOR_UPDATE:
-        // Show "Checking for update" notification
+  codePushStatusDidChange(syncStatus) {
+    switch (syncStatus) {
+      case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+        this.setState({syncMessage: 'Checking for update.'});
         break;
-      case SyncStatus.AWAITING_USER_ACTION:
-        // Show "Checking for update" notification
+      case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+        this.setState({syncMessage: 'Downloading package.'});
         break;
-      case SyncStatus.DOWNLOADING_PACKAGE:
-        // Show "downloading" notification
+      case CodePush.SyncStatus.AWAITING_USER_ACTION:
+        this.setState({syncMessage: 'Awaiting user action.'});
         break;
-      case SyncStatus.INSTALLING_UPDATE:
-        // Show "installing" notification
+      case CodePush.SyncStatus.INSTALLING_UPDATE:
+        this.setState({syncMessage: 'Installing update.'});
         break;
-      case SyncStatus.UPDATE_INSTALLED:
-        RNRestart.Restart();
+      case CodePush.SyncStatus.UP_TO_DATE:
+        this.setState({syncMessage: 'App up to date.', progress: false});
+        break;
+      case CodePush.SyncStatus.UPDATE_IGNORED:
+        this.setState({
+          syncMessage: 'Update cancelled by user.',
+          progress: false,
+        });
+        break;
+      case CodePush.SyncStatus.UPDATE_INSTALLED:
+        this.setState({
+          syncMessage: 'Update installed and will be applied on restart.',
+          progress: false,
+        });
+        break;
+      case CodePush.SyncStatus.UNKNOWN_ERROR:
+        this.setState({
+          syncMessage: 'An unknown error occurred.',
+          progress: false,
+        });
         break;
     }
-  };
-
-  onDownloadProgress = downloadProgress => {
-    this.setState(
-      {
-        loading: true,
-        tolal: downloadProgress.totalBytes,
-        recived: downloadProgress.receivedBytes,
-      },
-      () => {
-        totalPercent = (
-          (parseFloat(this.state.recived) / parseFloat(this.state.tolal)) *
-          100
-        ).toFixed(2);
-      },
-    );
-
-    if (downloadProgress.receivedBytes === downloadProgress.totalBytes) {
-      this.time = setTimeout(() => {
-        RNRestart.Restart();
-      }, 500);
-    }
-    if (downloadProgress) {
-      console.log(downloadProgress);
-      console.log(
-        'Downloading ' +
-          downloadProgress.receivedBytes +
-          ' of ' +
-          downloadProgress,
-      );
-    }
-  };
-  async UNSAFE_componentWillMount() {
-    await this.checkUpdateApp();
   }
 
-  checkUpdateApp = async () => {
-    this.setState({loading: false});
-    await CodePush.sync(
+  codePushDownloadDidProgress(progress) {
+    this.setState({progress});
+  }
+
+  toggleAllowRestart() {
+    this.state.restartAllowed
+      ? CodePush.disallowRestart()
+      : CodePush.allowRestart();
+
+    this.setState({restartAllowed: !this.state.restartAllowed});
+  }
+
+  getUpdateMetadata() {
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING).then(
+      (metadata: LocalPackage) => {
+        this.setState({
+          syncMessage: metadata
+            ? JSON.stringify(metadata)
+            : 'Running binary version',
+          progress: false,
+        });
+      },
+      (error: any) => {
+        this.setState({syncMessage: 'Error: ' + error, progress: false});
+      },
+    );
+  }
+
+  /** Update is downloaded silently, and applied on restart (recommended) */
+  sync() {
+    CodePush.sync(
+      {},
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this),
+    );
+  }
+
+  /** Update pops a confirmation dialog, and then immediately reboots the app */
+  syncImmediate() {
+    CodePush.sync(
       {
+        installMode: CodePush.InstallMode.IMMEDIATE,
         updateDialog: {
           optionalInstallButtonLabel: 'Cài đặt',
           optionalIgnoreButtonLabel: 'Bỏ qua',
           title: 'Cập nhật có sẵn',
           optionalUpdateMessage: 'Đã có bản cập nhật, bạn có muốn cài đặt nó?',
         },
-        installMode: CodePush.InstallMode.IMMEDIATE,
       },
-      this.onSyncStatusChange,
-      this.onDownloadProgress,
-    );
-    this.setState({loading: false});
-  };
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this),
+    ).then(r => console.log(r));
+  }
 
   render() {
-    return this.state.loading ? (
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: Colors.white,
-          flex: 1,
-        }}>
-        <Text>Cập nhật phiên bản mới ! </Text>
-        <Progress.Pie
-          progress={totalPercent / 100}
-          radius={55}
-          borderWidth={2}
-          color={Colors.primary}
-          size={120}
-          bgColor="#fff">
-          <Text style={{fontSize: 20, textAlign: 'center'}}>
-            {totalPercent}
-            {'%'}{' '}
+    let progressView;
+
+    if (this.state.progress) {
+      progressView = (
+        <Text style={styles.messages}>
+          {this.state.progress.receivedBytes} of{' '}
+          {this.state.progress.totalBytes} bytes received
+        </Text>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          CodePush! Anh An thuc hien 2021/07/17
+        </Text>
+        <TouchableOpacity onPress={this.sync.bind(this)}>
+          <Text style={styles.syncButton}>Press for background sync</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.syncImmediate.bind(this)}>
+          <Text style={styles.syncButton}>Press for dialog-driven sync</Text>
+        </TouchableOpacity>
+        {progressView}
+        <Image
+          style={styles.image}
+          resizeMode={'contain'}
+          source={require('./images/laptop_phone_howitworks.png')}
+        />
+        <TouchableOpacity onPress={this.toggleAllowRestart.bind(this)}>
+          <Text style={styles.restartToggleButton}>
+            Restart {this.state.restartAllowed ? 'allowed' : 'forbidden'}
           </Text>
-        </Progress.Pie>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.getUpdateMetadata.bind(this)}>
+          <Text style={styles.syncButton}>Press for Update Metadata</Text>
+        </TouchableOpacity>
+        <Text style={styles.messages}>{this.state.syncMessage || ''}</Text>
       </View>
-    ) : (
-      <SafeAreaView style={{backgroundColor: 'white'}}>
-        <StatusBar barStyle={'light-content'} />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={{backgroundColor: 'white'}}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: Colors.white,
-            }}>
-            <Section title="Anh An hom nay la manh thu">
-              Edit <Text style={styles.highlight}>App.js</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="Thai Xoan yeu duoi - 1 con vit">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Nguyen Van Du ư ư">
-              <DebugInstructions />
-            </Section>
-            <Section title={'Update App'}>
-              <TouchableOpacity onPress={() => this.checkUpdateApp()}>
-                <Text>Update App</Text>
-              </TouchableOpacity>
-            </Section>
-            <Section title="Trung Huong hôm nay đi hương trủng">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  image: {
+    margin: 30,
+    width: Dimensions.get('window').width - 100,
+    height: (365 * (Dimensions.get('window').width - 100)) / 651,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  messages: {
+    marginTop: 30,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  restartToggleButton: {
+    color: 'blue',
+    fontSize: 17,
+  },
+  syncButton: {
+    color: 'green',
+    fontSize: 17,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 20,
   },
 });
-let codePushOptions = {
-  updateDialog: true,
-  checkFrequency: CodePush.CheckFrequency.IMMEDIATE,
-};
+
+/**
+ * Configured with a MANUAL check frequency for easy testing. For production apps, it is recommended to configure a
+ * different check frequency, such as ON_APP_START, for a 'hands-off' approach where CodePush.sync() does not
+ * need to be explicitly called. All options of CodePush.sync() are also available in this decorator.
+ */
+let codePushOptions = {checkFrequency: CodePush.CheckFrequency.MANUAL};
+
+App = CodePush(codePushOptions)(App);
+
 export default App;
--- android/app/src/main/java/com/codepushdemo/MainApplication.java
+++ android/app/src/main/java/com/codepushdemo/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -31,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+//           packages.add(new MainReactPackage());
+//           packages.add(new CodePush(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), BuildConfig.DEBUG));
           return packages;
         }
 
